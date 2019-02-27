3a9c7464e67570716037df88134c0908d47a4594
Ryan Pope <poperyan73@gmail.com>
2019-02-26 20:50:42 -0600

    Added images for the slideshow

--- js/ex2.js
+++ js/ex2.js
@@ -1,5 +1,15 @@
+// Photo citations
+// https://pixabay.com/photos/puppy-dog-pet-animal-cute-white-1903313/
+// https://pixabay.com/photos/pug-puppy-dog-animal-cute-690566/
+// https://pixabay.com/photos/dog-young-dog-small-dog-maltese-1123016/
+// https://pixabay.com/photos/australian-shepherd-dog-animal-3237735/
+// https://pixabay.com/photos/dog-canine-samoyed-white-beach-699620/
 let images = [
-
+    "img/dog1.jpg",
+    "img/dog2.jpg",
+    "img/dog3.jpg",
+    "img/dog4.jpg",
+    "img/dog5.jpg" 
 ];
 
 let currentIndex = 0;
@@ -12,6 +22,8 @@ function next() {
   }
 
   document.getElementById("slideshow").src = images[currentIndex];
+
+  updateImage(images[currentIndex]);
 }
 
 function previous() {
@@ -22,11 +34,16 @@ function previous() {
   }
 
   document.getElementById("slideshow").src = images[currentIndex];
+
+  updateImage(images[currentIndex]);
 }
 
 function updateImage(name) {
   let img = document.getElementById("slideshow");
   img.src = name;
-  img.width = 0;
-  img.height = 0;
+  img.width = 960;
+  img.height = 540;
 }
+
+
+updateImage(images[0]);
